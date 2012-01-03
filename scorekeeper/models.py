689034c34aa3fabfaf8b83b8843457dba62c26f1
devnewton <devnewton>
2012-01-03 09:22:10 +0000

    registered user support

--- scorekeeper/models.py
+++ scorekeeper/models.py
@@ -29,7 +29,8 @@ class Level(models.Model):
 
 class Player(models.Model):
     slug = models.SlugField(max_length=80, unique=True)
-    secret = models.SlugField(max_length=80, unique=True)
+    secret = models.SlugField(max_length=80)
+    registered = models.BooleanField(default=False)
     @models.permalink
     def get_absolute_url(self):
         return ('scorekeeper.views.player_detail', [str(self.slug)])
@@ -39,7 +40,7 @@ class Player(models.Model):
         return self.score_set.all().order_by('-level__name', '-score')
     @staticmethod
     def cleanup():
-        for player in Player.objects.annotate(num_scores = models.Count('score')).filter(num_scores = 0):
+        for player in Player.objects.annotate(num_scores = models.Count('score')).filter(num_scores = 0, registered=False):
             player.delete()
 
 class Score(models.Model):
